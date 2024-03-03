6a8ae21bdbbfb94cf7aa6b1d007dad75f4b595aa
Mahammad <mehemmedabbasov123@gmail.com>
2024-03-03 15:37:07 +0400

    changes

--- assets/javascript/database.js
+++ assets/javascript/database.js
@@ -1,5 +1,37 @@
-import db  from "/assets/javascript/script.js"
-import {get, onValue, remove, ref, set, child} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+import {get,remove, ref, set} from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+
+
+
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCzAFtgFwvxbvW6xe7uYdMubdPO4JoYtWc",
+//   authDomain: "rockpaperscissors-1d8da.firebaseapp.com",
+//   projectId: "rockpaperscissors-1d8da",
+//   storageBucket: "rockpaperscissors-1d8da.appspot.com",
+//   messagingSenderId: "301698218341",
+//   appId: "1:301698218341:web:2c3a60a46a2b0d493c098c",
+//   measurementId: "G-C5N5XSPK7K"
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDdA6aeQq6g-DXeBRo_UXuPf5gsvefy_Qk",
+  authDomain: "chatbot-3410b.firebaseapp.com",
+  databaseURL: "https://chatbot-3410b-default-rtdb.firebaseio.com",
+  projectId: "chatbot-3410b",
+  storageBucket: "chatbot-3410b.appspot.com",
+  messagingSenderId: "918085028143",
+  appId: "1:918085028143:web:cf530fa8d4053b005539da",
+  measurementId: "G-XF1MWZ2PXJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+
+
 
 // adding formats
 const firstUserNameAddingButton = document.querySelector('#setFirstUserName');
@@ -323,11 +355,11 @@ selectelementForUserFirst.forEach(function(item){
 
 // Clear database on closing window
 
-// window.addEventListener('load', async function() {
-//     try {
-//         await remove(ref(db), 'melumat');
-//     } catch (error) {
-//         console.error('Error occurred while saving data:', error);
-//     }
-// });
+window.addEventListener('load', async function() {
+    try {
+        await remove(ref(db), 'melumat');
+    } catch (error) {
+        console.error('Error occurred while saving data:', error);
+    }
+});
 
